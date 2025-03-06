eaba662a1cf197c2048114a842a3f332724328f6
tugas <aldayanuar6@gmail.com>
2025-03-06 13:21:48 +0700

    kuis 1 Yanuar Alda Baran

---
+++ Barang25.java
@@ -0,0 +1,13 @@
+public class Barang25{
+    String kode;
+    String nama;
+    double harga;
+    int stok;
+
+    public Barang25(String kode, String nama, double harga, int stok) {
+        this.kode = kode;
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+}
